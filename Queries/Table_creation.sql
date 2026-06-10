de21724fbc98b05bba277f09874d9a00daa41623
Siva Prasad D <dasarisivaprasad4631@gmail.com>
2026-06-10 22:11:27 +0530

    Add initial database setup and salary data
    - Created a new database named `50_Days_Challenge_Sql`.
    - Added SQL scripts for creating tables: `departments`, `employees`, `salaries`, `attendance`, and `performance`.
    - Introduced a CSV file containing salary data for employees, including various salary entries with dates.

---
+++ Queries/Table_creation.sql
@@ -0,0 +1,33 @@
+use 50_Days_Challenge_Sql;
+
+CREATE TABLE 
+departments(dept_id varchar(40),
+dept_name varchar(40));
+
+CREATE TABLE employees
+(
+emp_id varchar(40),
+emp_name varchar(40),
+age varchar(40),
+city varchar(40),
+dept_id varchar(40),
+hire_date varchar(40));
+
+CREATE TABLE salaries
+(salary_id varchar(40),
+emp_id varchar(40),
+salary varchar(40),
+salary_date varchar(40));
+
+CREATE TABLE attendance
+(attendance_id varchar(40),
+emp_id varchar(40),
+attendance_date varchar(40),
+attendance_status varchar(40));)
+
+CREATE TABLE performance
+(
+    emp_id varchar(40),
+    rating_2022 varchar(40),
+    rating_2023 varchar(40),
+    rating_2024 varchar(40));
---
+++ Queries/database_creation.sql
@@ -0,0 +1,2 @@
+create database 50_Days_Challenge_Sql;
+
